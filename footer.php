2814e382fa23c0078a12ee6ea191c5c896b1b5d3
DevHouse <service@devhouse.se>
2021-07-15 08:45:56 +0000

    Basic templating for footer.php & styling completed.

--- footer.php
+++ footer.php
@@ -33,14 +33,14 @@
 					<div class="row d-flex justify-content-between">
 						
 						<!-- footer column 1 -->
-						<div class="col-md-4 footer-meta">
+						<div class="col-lg-4 footer-meta">
 							
 							<?php if (get_field('footer_about_text', 'option')) : ?>
 								<p class="footer-about"><?php echo get_field('footer_about_text', 'option'); ?></p>
 							<?php endif; ?>
 							
 							<?php if (get_field('footer_copyright_text', 'option')) : ?>
-								<p class="copyright">© <?php echo date("Y"); ?> <?php echo get_field('footer_copyright_text', 'option'); ?></p>
+								<p class="copyright mobile-hidden tablet-hidden">© <?php echo date("Y"); ?> <?php echo get_field('footer_copyright_text', 'option'); ?></p>
 							<?php endif; ?>
 								
 							<?php if (get_field('footer_link_url', 'option') && get_field('footer_link_text', 'option')) : ?>	
@@ -50,7 +50,7 @@
 						</div>
 						
 						<!-- footer column 2 -->
-						<div class="col-md-4 footer-menu">
+						<div class="col-lg-4 footer-menu">
 							<?php
 							wp_nav_menu(
 								array(
@@ -62,32 +62,56 @@
 						</div>
 						
 						<!-- footer column 3 -->
-						<div class="col-md-3 footer-contact">
+						<div class="col-lg-3 footer-contact">
 							
 							<?php if (get_field('footer_phone_number', 'option')) : ?>
-								<p class="phone">
-									<a href="tel:<?php echo get_field('footer_phone_number', 'option'); ?>">
-										
-										<?php echo get_field('footer_phone_number', 'option'); ?>
-									</a>
-								</p>
+								<a class="phone" href="tel:<?php echo get_field('footer_phone_number', 'option'); ?>">
+									<p><?php echo get_field('footer_phone_number', 'option'); ?></p>
+								</a>
 							<?php endif; ?>
 							
 							<?php if (get_field('footer_e-mail', 'option')) : ?>
-								<p class="email">
-									<a href="mailto:<?php echo get_field('footer_e-mail', 'option'); ?>">
-										<i src="/wp-content/themes/next_state/inc/images/email-icon.ico"></i>
-										<?php echo get_field('footer_e-mail', 'option'); ?>
-									</a>
-								</p>
+								<a class="email" href="mailto:<?php echo get_field('footer_e-mail', 'option'); ?>">
+									<p><?php echo get_field('footer_e-mail', 'option'); ?></p>
+								</a>
 							<?php endif; ?>
 					
 						</div>
 						
 						<!-- footer column 4 -->
-						<div class="col-md-1 footer-social">
-							link<br>
-							icon
+						<div class="col-lg-1 footer-social">
+							<?php
+
+								// Check rows exists.
+								if( have_rows('social_media_links', 'option') ):
+								
+								    // Loop through rows.
+								    while( have_rows('social_media_links', 'option') ) : the_row();
+								
+								        // Load sub field value.
+								        $icon_link = get_sub_field('social_media_url');
+								        $icon_image = get_sub_field('social_media_image_icon_url');
+								        // Do something...
+								        
+								        echo '<a class="bg-image" target="_blank" href="' . $icon_link . '" style="background-image: url(' . $icon_image . ');"></a>';
+								
+								    // End loop.
+								    endwhile;
+								
+								// No value.
+								else :
+								    // Do something...
+								endif;
+								
+							?>	
+						</div>
+						
+						<div class="col-12 desktop-hidden">
+							
+							<?php if (get_field('footer_copyright_text', 'option')) : ?>
+								<p class="copyright">© <?php echo date("Y"); ?> <?php echo get_field('footer_copyright_text', 'option'); ?></p>
+							<?php endif; ?>
+								
 						</div>
 						
 					</div>
